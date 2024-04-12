b01f432b84dcd9d4cadc8218d755cd15eaca40e6
Anderley Candela <anderleyc@gmail.com>
2024-04-12 12:54:23 -0500

    fix files

--- App.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-  import ToDo from './view/ToDo.vue';
-</script>
-
-<template>
-  <ToDo />
-</template>
-
-<style scoped></style>
--- types/task.ts
+++
@@ -1,8 +0,0 @@
-export interface Task {
-isEditing: boolean;
-showDropdown: boolean;
-  id: number;
-  description: string;
-  done: boolean;
-  createdAt: Date;
-}
